f805aed48f4e4d4d3237fc2d064dcc894820b804
泽阳 <mail@zhoumq.cn>
2017-08-16 11:46:18 +0800

    添加lowdb数据库

--- renderer-process/zfy/camera.js
+++ renderer-process/zfy/camera.js
@@ -3,24 +3,30 @@ const video = document.getElementById('video'),
         snap = document.getElementById('tack'),
         img = document.getElementById('img'),
         btn_submit = document.getElementById('btn-submit'),
+        btn_idcard = document.getElementById('btn-idcard'),
+        low = require('lowdb'),
+        db = low('db.json'),
+        btn_name = document.getElementById('btn-name'),
+        base64Img = require('base64-img'),
         vendorUrl = window.URL || window.webkitURL;
+        db.defaults({ zfy:{} }).write();
 
-    //媒体对象
-    navigator.getMedia = navigator.getUserMedia ||
-                         navagator.webkitGetUserMedia ||
-                         navigator.mozGetUserMedia ||
-                         navigator.msGetUserMedia;
-    navigator.getMedia({
-        video: true, //使用摄像头对象
-        audio: false  //不适用音频
-    }, function(strem){
-        console.log(strem);
-        video.src = vendorUrl.createObjectURL(strem);
-        video.play();
-    }, function(error) {
-        //error.code
-        console.log(error);
-    });
+      //媒体对象
+      navigator.getMedia = navigator.getUserMedia ||
+                           navagator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia;
+      navigator.getMedia({
+          video: true, //使用摄像头对象
+          audio: false  //不适用音频
+      }, function(strem){
+          video.src = vendorUrl.createObjectURL(strem);
+          video.play();
+      }, function(error) {
+          //error.code
+          alert('调用摄像头出错,请检查摄像头是否正常连接到计算机');
+      });
+
     snap.addEventListener('click', function(){
 
         //绘制canvas图形
@@ -29,7 +35,47 @@ const video = document.getElementById('video'),
         //把canvas图像转为img图片
         img.src = canvas.toDataURL("image/png");
 
+        img.onload =function() {
+          var data = base64Img.base64Sync(img.src);
+          console.log(data);
+        }
     });
+
     btn_submit.addEventListener('click',()=>{
       alert('Hello Electron!');
+    });
+
+    btn_idcard.addEventListener('click',()=>{
+        let input_idcard = document.getElementById('input-idcard');
+        let value = input_idcard.value;
+        try{
+          db.get('zfy')
+            .set('zfy.idcard',value)
+            .write();
+            alert('添加体检人员身份证成功');
+            let parent = document.getElementById('name-demo-toggle').parentElement;
+            document.getElementById('idcard-demo-toggle').parentElement.classList.toggle('is-open');
+            parent.classList.toggle('is-open');
+        }catch(err){
+          alert('添加体检人员身份证出错');
+        }
+        // let v = db.get('zfy')
+        //   .find({idcard: value})
+        //   .value();
+    });
+
+    btn_name.addEventListener('click',()=>{
+      let input_name = document.getElementById('input-name');
+      let value = input_name.value;
+      try{
+        db.get('zfy')
+          .set('zfy.name',value)
+          .write();
+          alert('添加体检人员姓名成功');
+          let parent = document.getElementById('camera-demo-toggle').parentElement;
+          document.getElementById('name-demo-toggle').parentElement.classList.toggle('is-open');
+          parent.classList.toggle('is-open');
+      }catch(err){
+        alert('添加体检人员姓名出错');
+      }
     });
